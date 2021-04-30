2195c1371e5f8db7ff17e6a4ba3e477b7dbb0889
TBC <49328664+tbchhetri@users.noreply.github.com>
2021-04-30 19:50:20 -0400

    documented

--- ModelApplication.py
+++ ModelApplication.py
@@ -1,4 +1,5 @@
-#try try try
+#uses the model developed from the machine learning algorithm to control the steering of
+#the car by send pwm signal to the steering motor of the car
 import numpy as np
 import cv2
 import tensorflow as tf
@@ -9,11 +10,13 @@ import matplotlib.image as mpimg
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
+#this is the model output from the ml algorithm
 model = tf.keras.models.load_model("model.h5")
 #print(model.summary())
 
-image = cv2.imread('road3.jpg') #this reads the image and returns it as an array with each pixel's values
+#image = cv2.imread('road3.jpg') #this reads the image and returns it as an array with each pixel's values
 
+#this function preprocess all the incoming images and makes them like the image we trained the ML algo with
 def img_preprocess(img):
   #img = mpimg.imread(img)
   img = img[60:135,:,:] #we are cropping the height of the image since other than 60-135 we dont need those infos
@@ -31,12 +34,13 @@ enA = 13
 in1 = 2
 in2 = 3
 #for right one
-enB = 12
-in3 = 17
-in4 = 27
+#enB = 12
+#in3 = 17
+#in4 = 27
 
-error = 100
+error = 100 #the proportional controller
 
+#required gpio setup for the RPi
 GPIO.setup(enA, GPIO.OUT)
 GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
@@ -44,16 +48,16 @@ GPIO.setup(in2, GPIO.OUT)
 pwmA = GPIO.PWM(enA, 100)
 pwmA.start(0)
 
-GPIO.setup(enB, GPIO.OUT)
-GPIO.setup(in3, GPIO.OUT)
-GPIO.setup(in4, GPIO.OUT)
+#GPIO.setup(enB, GPIO.OUT)
+#GPIO.setup(in3, GPIO.OUT)
+#GPIO.setup(in4, GPIO.OUT)
 
-pwmB = GPIO.PWM(enA, 100)
-pwmB.start(0)
+#pwmB = GPIO.PWM(enA, 100)
+#pwmB.start(0)
 
 cap = cv2.VideoCapture("ohiotest.mp4")
 while (cap.isOpened()):
-    _, frame = cap.read()
+    _, frame = cap.read() #extracts every frame from the image and saves it in frame
 
     img = img_preprocess(frame)
 
